bdc4e37f2cc96706dd7aeda494c927e981c2151f
Masih Shafieian <masih.shafieian@gmail.com>
2021-06-21 16:25:41 +0100

    boost and rotation added

--- Rocket Boost/Assets/Movment.cs
+++ Rocket Boost/Assets/Movment.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Movment : MonoBehaviour
 {
+    [SerializeField] float boostThrust = 100f;
+    [SerializeField] float rotateThrust = 100f;
+    Rigidbody myRigidBody;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {        
+        myRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Boost();
+        Rotation();
+    }
+
+    void Boost()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+           myRigidBody.AddRelativeForce(Vector3.up* boostThrust * Time.deltaTime);
+        }        
+    }
+
+    void Rotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward*rotateThrust*Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward*rotateThrust*Time.deltaTime);
+
+        }
     }
 }
